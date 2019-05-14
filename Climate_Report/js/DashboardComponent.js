6e6145c6a639227b351c32d63978e5654f58cb31
lessthan 41 <b06208030@ntu.edu.tw>
2019-05-14 21:09:44 +0800

    dataUpdate2

--- Climate_Report/js/DashboardComponent.js
+++ Climate_Report/js/DashboardComponent.js
@@ -5,11 +5,7 @@ class DashboardComponent {
   }
 
   init () {
-    let SO2Chart = new Chart(this.AQI, '#SO2PerHourChart', 'SO2', ['July', 'November'], 20);
-    SO2Chart.render();
-    let O3Chart = new Chart(this.AQI, '#O3PerHourChart', 'O3', ['July', 'November']);
-    O3Chart.render();
-    let PM25Chart = new Chart(this.AQI, '#PM25PerHourChart', 'PM25', ['July', 'November']);
-    PM25Chart.render();
+    let NOXChart = new Chart(this.AQI, '#NOXPerHourChart', 'NOX', ['CarNumber', 'NOx']);
+    NOXChart.render();
   }
 }
--- Climate_Report/js/index.js
+++ Climate_Report/js/index.js
@@ -1,14 +1,9 @@
 
 (function () {
 
-  d3.dsv(',', 'https://raw.githubusercontent.com/lessthan41/D3_Practice/master/Climate_HW8/toPlotQ1.csv', function (row) {
+  d3.dsv(',', 'https://raw.githubusercontent.com/lessthan41/D3_Practice/master/Climate_Report/data/airdata_0905.csv', function (row) {
     return {
-      NOx: +row['SO2.7.Q1'],
-      O3_7: +row['O3.7.Q1'],
-      PM25_7: +row['PM2.5.7.Q1'],
-      SO2_11: +row['SO2.11.Q1'],
-      O3_11: +row['O3.11.Q1'],
-      PM25_11: +row['PM2.5.11.Q1']
+      NOx: +row['NOx (ppb)']
     };
   }).then(function (AQI) {
     let dashboard = new DashboardComponent(AQI);
